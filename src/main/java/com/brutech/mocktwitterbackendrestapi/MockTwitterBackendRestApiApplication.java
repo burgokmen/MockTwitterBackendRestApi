0a808820d188a494cc1b8fb26d360a10713f9d7f
burgokmen <burgokmen@gmail.com>
2023-11-04 22:37:52 +0300

    init

---
+++ src/main/java/com/brutech/mocktwitterbackendrestapi/MockTwitterBackendRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.brutech.mocktwitterbackendrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockTwitterBackendRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockTwitterBackendRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brutech/mocktwitterbackendrestapi/MockTwitterBackendRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brutech.mocktwitterbackendrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockTwitterBackendRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
