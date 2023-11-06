e71ee72c2195a97c0f7bd5353a5b9bd5791142a4
burgokmen <burgokmen@gmail.com>
2023-11-06 06:54:29 +0300

    tweet repo done

--- src/main/java/com/brutech/mocktwitterbackendrestapi/repository/TweetRepository.java
+++ src/main/java/com/brutech/mocktwitterbackendrestapi/repository/TweetRepository.java
@@ -2,6 +2,15 @@ package com.brutech.mocktwitterbackendrestapi.repository;
 
 import com.brutech.mocktwitterbackendrestapi.entity.Tweet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
+
+
+    @Query("SELECT t FROM Tweet t " +
+            "INNER JOIN t.profile p " +
+            "WHERE p.id = :id")
+    public List<Tweet> getTweetByUserId(int id);
 }
