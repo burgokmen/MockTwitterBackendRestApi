dd57aff4b6e9519b5f94ef700fafe8cfec6042f0
burgokmen <burgokmen@gmail.com>
2023-11-06 06:40:56 +0300

    response update

--- src/main/java/com/brutech/mocktwitterbackendrestapi/dto/ProfileResponse.java
+++ src/main/java/com/brutech/mocktwitterbackendrestapi/dto/ProfileResponse.java
@@ -4,5 +4,5 @@ import java.util.Date;
 
 public record ProfileResponse(String userHandle, String firstName, String lastName,
                               String email ,String password, String cellular,
-                              Date birthday, Date createdAt, String location, String profilePicture ) {
+                              String birthday, String createdAt, String location, String profilePicture ) {
 }
--- src/main/java/com/brutech/mocktwitterbackendrestapi/dto/TweetResponse.java
+++ src/main/java/com/brutech/mocktwitterbackendrestapi/dto/TweetResponse.java
@@ -2,6 +2,6 @@ package com.brutech.mocktwitterbackendrestapi.dto;
 
 import java.util.Date;
 
-public record TweetResponse(String tweetBody, long commentsNumber, long retweetsNumber,
-                            long likesNumber, Date createdAt, ProfileTweetResponse profileTweetResponse){
+public record TweetResponse(long tweetId, String tweetBody, long commentsNumber, long retweetsNumber,
+                            long likesNumber, String createdAt, ProfileTweetResponse profileTweetResponse){
 }
