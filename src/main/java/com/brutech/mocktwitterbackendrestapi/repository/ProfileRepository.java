d335c0aea523d74917a4b7b8b170f29e05ffc347
burgokmen <burgokmen@gmail.com>
2023-11-09 04:14:32 +0300

    Repo update

--- src/main/java/com/brutech/mocktwitterbackendrestapi/repository/ProfileRepository.java
+++ src/main/java/com/brutech/mocktwitterbackendrestapi/repository/ProfileRepository.java
@@ -3,6 +3,9 @@ package com.brutech.mocktwitterbackendrestapi.repository;
 import com.brutech.mocktwitterbackendrestapi.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
 
@@ -14,4 +17,9 @@ public interface ProfileRepository extends JpaRepository<Profile, Long> {
 
     @Query(value = "SELECT p.username FROM twitter.profile AS p WHERE p.username =:username", nativeQuery = true)
     String usernameChecker(String username);
+
+
+
+    @Query("SELECT p FROM Profile p WHERE p.email =:email")
+    Optional<Profile> getUserByEmail(@Param("email") String email);
 }
