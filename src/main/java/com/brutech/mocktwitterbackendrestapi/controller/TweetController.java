1ff69a51e13ca6fbb3ae401b694f4d639103982d
burgokmen <burgokmen@gmail.com>
2023-11-14 02:50:48 +0300

    Tweet controller updated with new post methods

--- src/main/java/com/brutech/mocktwitterbackendrestapi/controller/TweetController.java
+++ src/main/java/com/brutech/mocktwitterbackendrestapi/controller/TweetController.java
@@ -6,7 +6,6 @@ import com.brutech.mocktwitterbackendrestapi.entity.Tweet;
 import com.brutech.mocktwitterbackendrestapi.service.ProfileService;
 import com.brutech.mocktwitterbackendrestapi.service.TweetService;
 import com.brutech.mocktwitterbackendrestapi.util.Converter;
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -58,6 +57,52 @@ public class TweetController {
     public TweetResponse deleteTweet(@PathVariable Long id){
         return Converter.tweetResponseConverter(tweetService.deleteTweet(id));
     }
+
+    @PostMapping("/like/{id}")
+    public TweetResponse likeTweet(@PathVariable Long id, @RequestBody Profile profile){
+        Tweet tweet = tweetService.getTweetById(id);
+       Profile profile1 = profileService.getUserById(profile.getId());
+        profile1.addLikedTweetIds(tweet.getId());
+        tweet.addLikedByUserId(profile1.getId());
+        return Converter.tweetResponseConverter(tweetService.saveTweet(tweet));
+    }
+
+    @PostMapping("/unlike/{id}")
+    public TweetResponse unlikeTweet(@PathVariable Long id, @RequestBody Profile profile){
+        Tweet tweet = tweetService.getTweetById(id);
+        Profile profile1 = profileService.getUserById(profile.getId());
+        profile1.removeLikedTweetIds(tweet.getId());
+        tweet.removeLikedByUserId(profile1.getId());
+        return Converter.tweetResponseConverter(tweetService.saveTweet(tweet));
+    }
+
+        @PostMapping("/retweet/{id}")
+        public TweetResponse retweetTweet(@PathVariable Long id, @RequestBody Profile profile){
+            Tweet tweet = tweetService.getTweetById(id);
+            Profile profile1 = profileService.getUserById(profile.getId());
+            profile1.addRetweetsTweetsIdList(tweet.getId());
+            tweet.addRetweetedByUserIdList(profile1.getId());
+            return Converter.tweetResponseConverter(tweetService.saveTweet(tweet));
+        }
+
+        @PostMapping("/unretweet/{id}")
+        public TweetResponse unretweetTweet(@PathVariable Long id, @RequestBody Profile profile){
+            Tweet tweet = tweetService.getTweetById(id);
+            Profile profile1 = profileService.getUserById(profile.getId());
+            profile1.removeRetweetsTweetsIdList(tweet.getId());
+            tweet.removeRetweetedByUserIdList(profile1.getId());
+            return Converter.tweetResponseConverter(tweetService.saveTweet(tweet));
+        }
+
+        @PostMapping("/comment/{id}")
+        public TweetResponse commentTweet(@PathVariable Long id, @RequestBody Tweet tweet){
+            Tweet commentedTweet = tweetService.getTweetById(id);
+            Profile profile = profileService.getUserById(tweet.getProfile().getId());
+            tweet.setProfile(profile);
+            Tweet tweet1 = tweetService.saveTweet(tweet);
+            commentedTweet.addCommentedByTweetIdList(tweet1.getId());
+            return Converter.tweetResponseConverter(tweetService.saveTweet(tweet1));
+        }
 
 
 }
