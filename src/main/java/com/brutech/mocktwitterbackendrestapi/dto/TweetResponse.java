3d4ca6c2efa2a805679be1755e40e752dcd81a61
burgokmen <burgokmen@gmail.com>
2023-11-15 00:22:57 +0300

    tweet response updated accordingly with entity

--- src/main/java/com/brutech/mocktwitterbackendrestapi/dto/TweetResponse.java
+++ src/main/java/com/brutech/mocktwitterbackendrestapi/dto/TweetResponse.java
@@ -3,5 +3,8 @@ package com.brutech.mocktwitterbackendrestapi.dto;
 import java.util.Date;
 import java.util.List;
 
-public record TweetResponse(long tweetId, String tweetBody, String createdAt, ProfileTweetResponse profileTweetResponse, List<Long> likedByUserIdList, List<Long> commentedByTweetIdList, List<Long> retweetedByUserIdList){
+public record TweetResponse(long tweetId, String tweetBody,
+                            String createdAt, ProfileTweetResponse profileTweetResponse,
+                            List<Long> likedByUserIdList, List<Long> commentedByTweetIdList,
+                            List<Long> retweetedByUserIdList, Long commentedTweet){
 }
