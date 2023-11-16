988a6533ccf8efa6062fd30efb5d26465e794d65
burgokmen <burgokmen@gmail.com>
2023-11-16 10:31:39 +0300

    UPDATE global exception handler has a new method to show validation errors

--- src/main/java/com/brutech/mocktwitterbackendrestapi/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/brutech/mocktwitterbackendrestapi/exceptions/GlobalExceptionHandler.java
@@ -3,8 +3,15 @@ package com.brutech.mocktwitterbackendrestapi.exceptions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 @Slf4j
@@ -26,4 +33,30 @@ public class GlobalExceptionHandler {
         log.error("Exception: " + e.getMessage());
         return new ResponseEntity<>(errorResponse, org.springframework.http.HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler
+    public ResponseEntity<TwitterErrorResponse> handleBindExceptions(MethodArgumentNotValidException exception){
+        List<Map<String, String>> errorList = exception.getBindingResult().getFieldErrors()
+                .stream().map(fieldError -> {
+                    log.error(fieldError.getField() + ": " + fieldError.getDefaultMessage());
+                    Map<String, String> errors = new HashMap<>();
+                    errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+                    return errors;
+                }).collect(Collectors.toList());
+
+        StringBuilder errorMessage = new StringBuilder();
+        for (Map<String, String> map : errorList) {
+            for (String message : map.values()) {
+                if (!errorMessage.isEmpty()) {
+                    errorMessage.append(", ");
+                }
+                errorMessage.append(message);
+            }
+        }
+
+        TwitterErrorResponse twitterErrorResponse = new TwitterErrorResponse(HttpStatus.BAD_REQUEST.value(), errorMessage.toString(), LocalDateTime.now());
+        return ResponseEntity.badRequest().body(twitterErrorResponse);
+    }
+
+
 }
