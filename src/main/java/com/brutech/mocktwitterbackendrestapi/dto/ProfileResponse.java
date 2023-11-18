e0e65f3f5a76fcc7ffc1cbcadf891680dc38b5c4
burgokmen <burgokmen@gmail.com>
2023-11-18 19:15:40 +0300

    UPDATE profile response now has p wallpaper

--- src/main/java/com/brutech/mocktwitterbackendrestapi/dto/ProfileResponse.java
+++ src/main/java/com/brutech/mocktwitterbackendrestapi/dto/ProfileResponse.java
@@ -4,5 +4,5 @@ package com.brutech.mocktwitterbackendrestapi.dto;
 public record ProfileResponse(Long id, String userHandle, String firstName, String lastName,
                               String email ,String password, String cellular,
                               String birthday, String createdAt,
-                              String location, String profilePicture ) {
+                              String location, String profilePicture, String profileWallpaper) {
 }
